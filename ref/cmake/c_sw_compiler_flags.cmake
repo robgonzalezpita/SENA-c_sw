844d241db1eddf18c4e83660959b994b2bcd6b4b
Rob Gonzalez-Pita <rob.gonzalez-pita@noaa.gov>
2020-12-08 13:24:17 -0700

    Add isEqual.90 and EqualTest.f90 files, ammend ref/src/CMakeLists.txt to include these files. Fix typo in ref/cmake/c_sw_compiler_flags.cmake file.

--- ref/cmake/c_sw_compiler_flags.cmake
+++ ref/cmake/c_sw_compiler_flags.cmake
@@ -2,14 +2,14 @@ if( NOT CMAKE_BUILD_TYPE MATCHES "Debug" )
   add_definitions( -DNDEBUG )
 endif( )
 
-# Set flags depending on which compiler iis used
+# Set flags depending on which compiler is used
 if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   include("compiler_flags_GNU_Fortran")
 elseif (CMAKE_Fortran_COMPILER_ID STREQUAL "Intel")
   include("compiler_flags_Intel_Fortran")
 endif()
 
-# Set flags depending on which compiler iis used
+# Set flags depending on which compiler is used
 if (CMAKE_C_COMPILER_ID STREQUAL "GNU")
   include("compiler_flags_GNU_C")
 elseif (CMAKE_C_COMPILER_ID STREQUAL "Intel")
